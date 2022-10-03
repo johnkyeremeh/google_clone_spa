974598814382a8787ea69593867687a1634f494e
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-10-03 14:39:07 -0700

    Connected search to component search. Fixing rerendering error when submit multiple times

--- front-end/components/search.js
+++ front-end/components/search.js
@@ -5,20 +5,26 @@ import Button from '@mui/material/Button';
 import '../css/search.css'
 import { setSearchTerm } from "../actions/search";
 import { hideSearchBtn } from "../actions/searchButtons";
+import { getSearchResults } from "../actions/search";
+
 function Search(props){
     const [input, setInput] = useState('')
     const navigate = useNavigate();
     
     const getSearch = (e) =>{
         e.preventDefault()
-        console.log("Search")
-        props.setSearchTerm(input)
-        navigate("/search");
+        console.log("Search btn clicked")
+        // props.setSearchTerm(input)
+        setInput(props.search.term)
+        props.getSearchResults(props.search)
+        navigate(`/search?q=${props.search.term}`);
+       
         props.hideSearchBtn()
     }
 
     const onChange = (e) =>{
         setInput([e.target.name] = e.target.value)
+        props.setSearchTerm(e.target.value)
         console.log("change")
     }
 
@@ -26,13 +32,12 @@ function Search(props){
     <form>
         <div className="search">
             <div className="search--searchInput">
-                <input  onChange={onChange} placeholder={props.term}/>
+                <input  onChange={onChange} placeholder={input} value={input}/>
             </div>
         </div>
         {props.searchButtons ? (
             <div className="search--button">
             <Button onClick={getSearch}variant="outlined" type="submit">Google Search</Button>
-            <Button variant="outlined" type="submit">I'm Feeling Lucky</Button>
          </div>
         ) : null }
         
@@ -50,7 +55,7 @@ const mapStatetoProps = (state) => {
   }
 }
 
-export default connect(mapStatetoProps, {setSearchTerm, hideSearchBtn})(Search)
+export default connect(mapStatetoProps, {setSearchTerm, hideSearchBtn, getSearchResults})(Search)
 
 
 
