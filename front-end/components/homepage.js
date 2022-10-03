2f366853ebec02ee0526ee1c6f18988c338dbd09
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-10-03 14:37:23 -0700

    Replaced Link to Anchor to link to external domains

--- front-end/components/homepage.js
+++ front-end/components/homepage.js
@@ -8,13 +8,14 @@ function Homepage(){
         <div className="homepage">
             <div className="homepage--header">
                 <div className="homepage--left">
-                    <Link className="homepage--link" to="/about">About</Link>
-                    <Link className="homepage--link" to="/store">Store</Link>
+                    <a  className="homepage--link"  target="_blank" href="https://about.google/?fg=1&utm_source=google-CA&utm_medium=referral&utm_campaign=hp-header">About</a>
+                    <a  className="homepage--link"  target="_blank" href="https://store.google.com/CA?utm_source=hp_header&utm_medium=google_ooo&utm_campaign=GS100042&hl=en-CA">Store</a>
+                   
                 </div>
 
                 <div className="homepage--right">
-                    <Link className="homepage--link" to="/gmail">Gmail</Link>
-                    <Link className="homepage--link" to="/images">Images</Link>
+                <a  className="homepage--link"  target="_blank" href="https://mail.google.com/mail/&ogbl">Gmail</a>
+                <a  className="homepage--link"  target="_blank" href="https://www.google.ca/imghp?hl=en&ogblA">Images</a>
                 </div>
             </div>
             <div className="homepage--body">
