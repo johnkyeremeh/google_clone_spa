f08f6d4455a1a5f7499f164c39d56d349fa34854
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-10-03 14:49:22 -0700

    Created methods for clicking on next and previous buttons and rendered results to the screen

--- front-end/pages/searchResult.js
+++ front-end/pages/searchResult.js
@@ -1,20 +1,61 @@
 import React, {useState} from "react";
 import Search from "../components/search"
+import { getSearchResults, setShowPrevious,setShowNext  } from "../actions/search";
+import Button from '@mui/material/Button';
 import { connect } from "react-redux";
+import '../css/searchResults.css'
 
 function SearchResult(props){
     const [showButton, setShowButtons] = useState(false)
 
+    const data = props.search.data
+    const term = props.search.term
+
+   const showPrevious = () => {
+    console.log("show prev click")
+
+    props.getSearchResults(props.search)
+    props.setShowPrevious()
+
+   }
+
+   const showNext = () => {
+    console.log("show prev click")
+    props.getSearchResults(props.search)
+    props.setShowNext()
+   }
+
         return (
         <div className="searchResult">
-           < Search term={props.search.term}/>
-           {props.search.term}
+           < Search />
+           {term && (
            <div className="searchResults--results">
+                <div id="searchresultsarea">
+                    <p id="searchresultsnumber">About {data?.searchInformation.formattedTotalResults} results ({data?.searchInformation.formattedSearchTime}) seconds </p>
+                    {data?.items.map((item, index) => {
+                        return (
+                            <div key={index} className="searchresult">
+                               
+                                <a href={item.link}>{item.link}<br></br><h2>{item.title}</h2></a> 
+                                
+                                <p>{item.snippet}</p>
+                            </div>
+                        )})
+                    }
+                    
+                </div>
+                <div className="Pagination--button">
+                    <Button  variant="outlined" onClick={showPrevious}type="submit">Previous</Button>
+                    <Button variant="outlined" onClick={showNext} type="submit">Next</Button>
+                </div>
+            </div>)}
+        </div>  )
+}
+           
 
-           </div>
+        
+                
 
-        </div>)   
-} 
 	
 
 const mapStatetoProps = (state) => {
@@ -25,4 +66,4 @@ const mapStatetoProps = (state) => {
   }
 
   
-export default connect(mapStatetoProps)(SearchResult);
+export default connect(mapStatetoProps, {getSearchResults, setShowPrevious, setShowNext})(SearchResult);
