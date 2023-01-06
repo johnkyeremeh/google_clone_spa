3d020c690e0d76f6a742acd23ff440adb765965e
John Kyeremeh <john.a.kyeremeh@gmail.com>
2023-01-06 09:45:35 -0800

    Update for production

--- rptakehome/rptakehome/settings.py
+++ rptakehome/rptakehome/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
     }
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
